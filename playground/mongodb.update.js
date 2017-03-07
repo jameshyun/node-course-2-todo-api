a16464c1e707d5f73037e0eb8833c1d7e1a9a62e
james <sydjung@gmail.com>
2017-03-07 13:58:49 +1100

    Add update script

---
+++ playground/mongodb.update.js
@@ -0,0 +1,48 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('58bd0eeb60e2d6c2f272eb53')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('58bcfd7fcb9ac012c0c68b8d')
+  }, {
+    $set: {
+      name: 'James'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  // db.close();
+})
+
+
+
+/**
+  * methods can be seen in collection in the mongodb.github.io
+  * for updation, search for 'mongodb update operator' - $set
+ */
